0d1935bc77a3f826664dc7c9320a81098cda5e36
Zachary Rojas <ztr2194@gmail.com>
2023-03-29 19:51:06 -0500

    array filter lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, firstAnswer) {
+    if (firstAnswer = (drivers.filter(((drivers) => drivers.toLowerCase() === firstAnswer.toLowerCase())))) {
+        return firstAnswer;
+    }  
+}
+
+function fuzzyMatch(drivers, firstLetter) {
+    if (firstLetter = (drivers.filter(((drivers) => drivers.slice(0,2) === firstLetter)))) {
+        return firstLetter;
+    }  
+}
+
+function matchName(drivers, info) {
+    if (info = (drivers.filter(((drivers) => drivers.name === info)))) {
+        return info;
+    }  
+}
